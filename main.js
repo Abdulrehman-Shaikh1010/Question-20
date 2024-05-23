2b4e5b50e16510520f176763db6e831c26e010dd
Abdulrehman <you@example.com>
2024-05-23 10:49:50 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// Store a list of famous landmarks in an array
+var landmarks = [
+    "Eiffel Tower",
+    "Statue of Liberty",
+    "Taj Mahal",
+    "Great Wall of China",
+    "Machu Picchu",
+    "Colosseum",
+    "Sydney Opera House",
+    "Petra",
+    "Christ the Redeemer",
+    "Pyramids of Giza"
+];
+// Print the list of landmarks
+console.log("List of Famous Landmarks:");
+for (var i = 0; i < landmarks.length; i++) {
+    console.log(landmarks[i]);
+}
---
+++ main.ts
@@ -0,0 +1,19 @@
+// Store a list of famous landmarks in an array
+let landmarks: string[] = [
+    "Eiffel Tower",
+    "Statue of Liberty",
+    "Taj Mahal",
+    "Great Wall of China",
+    "Machu Picchu",
+    "Colosseum",
+    "Sydney Opera House",
+    "Petra",
+    "Christ the Redeemer",
+    "Pyramids of Giza"
+];
+
+// Print the list of landmarks
+console.log("List of Famous Landmarks:");
+for (let i = 0; i < landmarks.length; i++) {
+    console.log(landmarks[i]);
+}
